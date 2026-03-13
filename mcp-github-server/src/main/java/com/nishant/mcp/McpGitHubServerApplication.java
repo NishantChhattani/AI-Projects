640db35e703b3d624e1cc9817b76d97325fbcb70
NishantChhattani <nishant.chhattani@gmail.com>
2026-03-13 14:58:35 +0530

    Add main application class

---
+++ mcp-github-server/src/main/java/com/nishant/mcp/McpGitHubServerApplication.java
@@ -0,0 +1,12 @@
+package com.nishant.mcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+SpringBootApplication
+public class McpGitHubServerAppelication {
+
+    public static void main(String[] args) {
+        SpringAppelication.run(McpGitHubServerApplelication.class, args);
+    }
+}
